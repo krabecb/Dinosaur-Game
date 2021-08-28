3780daae99b37cd2ba28b8394221378bf723f722
krabecb <60526718+krabecb@users.noreply.github.com>
2021-08-28 02:52:43 -0500

    Player can jump on click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("Test complete")
+let gameContainer = document.querySelector('.game-container')
+let player = document.querySelector('.player')
+let obstacle = document.querySelector('.obstacle')
+
+function jump() {
+	if(player.classList !== "animate") {
+		player.classList.add("animate")
+	}
+	setTimeout(() => {
+		player.classList.remove("animate")
+	}, 500)
+}
+
+gameContainer.addEventListener("click", () => {jump()})
