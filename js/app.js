94862ad4106dd2edc0349c34fd59b361928dafc5
krabecb <60526718+krabecb@users.noreply.github.com>
2021-08-28 03:02:55 -0500

    Basic functionality complete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let gameContainer = document.querySelector('.game-container')
-let player = document.querySelector('.player')
-let obstacle = document.querySelector('.obstacle')
+const gameContainer = document.querySelector('.game-container')
+const player = document.querySelector('.player')
+const obstacle = document.querySelector('.obstacle')
 
 function jump() {
 	if(player.classList !== "animate") {
@@ -10,5 +10,15 @@ function jump() {
 		player.classList.remove("animate")
 	}, 500)
 }
+
+const checkCollision = setInterval(() => {
+	const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+	const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"))
+	if(obstacleLeft < 20 && obstacleLeft > 0 && playerTop >= 130) {
+		obstacle.style.animation = "none"
+		obstacle.style.display = "none"
+		alert("YOU LOSE!")
+	}
+},10)
 
 gameContainer.addEventListener("click", () => {jump()})
