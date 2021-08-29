9cedbffb5a872cd91fde0278e13001a4210cc523
krabecb <60526718+krabecb@users.noreply.github.com>
2021-08-28 21:17:07 -0500

    Refactored JS to move obstacle instead of CSS animation

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const player = document.querySelector('.player')
 const obstacle = document.querySelector('.obstacle')
 
 let counter = 0
+let obstaclePos = 480
+let gameOver = false
 
 const score = document.createElement("h3")
 score.classList.add("score")
@@ -24,50 +26,74 @@ function jump() {
 	}, 500)
 }
 
-const checkCollision = setInterval(() => {
-	const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
-	const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"))
-	if(obstacleLeft < 20 && obstacleLeft > 0 && playerTop >= 130) {
-		obstacle.style.animation = "none"
+function startTimer() {
+	const checkCollision = setInterval(() => {
+		const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+		let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"))
+		
+		obstacle.style.left = `${obstaclePos -= 2}px`
+		// console.log(obstaclePos)
 
-		const youLose = document.createElement("h1")
-		youLose.classList.add("you-lose")
-		youLose.innerHTML = "YOU LOST LMAO"
-		gameContainer.appendChild(youLose)
+		if(obstacleLeft < 20 && obstacleLeft > 0 && playerTop >= 130) {
+			// obstacle.style.animation = "none"
+			clearInterval(checkCollision)
+			gameOver = true
 
-		const restart = document.createElement("button")
-		restart.classList.add("restart")
-		restart.innerHTML = "Restart"
-		gameContainer.appendChild(restart)
+			const youLose = document.createElement("h1")
+			youLose.classList.add("you-lose")
+			youLose.innerHTML = "YOU LOST LMAO"
+			gameContainer.appendChild(youLose)
 
-		const restartLocation = document.querySelector(".restart")
-		restartLocation.addEventListener("click", () => {
-			counter = 0
-			score.innerHTML = `Points: ${counter}`
-			obstacle.style.animation = "obstacle 1s infinite linear"
-			const youLoseLocation = document.querySelector('.you-lose')
-			youLoseLocation.remove()
-			restartLocation.remove()
-		})
-	}
-},10)
+			const restart = document.createElement("button")
+			restart.classList.add("restart")
+			restart.innerHTML = "Restart"
+			gameContainer.appendChild(restart)
 
-const addScore = setInterval(() => {
-	const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
-	const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"))
-	if(obstacleLeft < 30 && playerTop <= 129) {
-		counter = counter += 1
-	}
-	score.innerHTML = `Points: ${counter}`
-},5)
+			const restartLocation = document.querySelector(".restart")
+			restartLocation.addEventListener("click", () => {
+				obstaclePos = 480
+				obstacle.style.left = `${obstaclePos}px`
+				counter = 0
+				score.innerHTML = `Points: ${counter}`
+				gameOver = false
+				// obstacle.style.animation = "obstacle 1s infinite linear"
+				const youLoseLocation = document.querySelector('.you-lose')
+				youLoseLocation.remove()
+				restartLocation.remove()
+				startTimer()
+				startScoreTracker()
+			})
+		}
+
+		if(obstaclePos === 0) {
+			obstaclePos = 480
+		}
+	},1)
+}
+
+function startScoreTracker() {
+	const addScore = setInterval(() => {
+		const playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"))
+		const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"))
+		if(obstacleLeft < 30 && playerTop <= 129 && gameOver === false) {
+			counter = counter += 1
+		} 
+		if(gameOver === true) {
+			clearInterval(addScore)
+		}
+		score.innerHTML = `Points: ${counter}`
+	},5)
+}
 
 
 
 
 const startButtonLocation = document.querySelector(".start-button")
 startButtonLocation.addEventListener("click", () => {
-	obstacle.classList.remove("obstacle")
-	obstacle.classList.add("obstacle-start")
+	startTimer()
+	startScoreTracker()
+	// obstacle.classList.remove("obstacle")
+	// obstacle.classList.add("obstacle-start")
 	startButtonLocation.style.display = "none"
 })
 
